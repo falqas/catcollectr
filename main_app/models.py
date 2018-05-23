c75b122d3e04849e730632fd55fd6d87e1ce7621
Faisal Al Qasimi <falqas@gmail.com>
2018-05-23 07:37:43 -0700

    add connection to Cat db

--- main_app/models.py
+++ main_app/models.py
@@ -6,6 +6,8 @@ from django.db import models
 
 
 class Cat(models.Model):
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from .models import Cat
 # Create your views here.
 
 from django.http import HttpResponse
 
 
 def index(request):
+    cats = Cat.objects.all()
     return render(request, 'index.html', {'cats': cats})
 
 
@@ -13,12 +14,12 @@ def about(request):
     return HttpResponse('<h1>About me page!</h1>')
 
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
 
 cats = [
