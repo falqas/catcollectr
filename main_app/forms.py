2e9c19cb54cdc41b6f1437e315f1110a09a11c93
Faisal Al Qasimi <falqas@gmail.com>
2018-05-23 13:24:44 -0700

    add working form which posts to db

---
+++ main_app/forms.py
@@ -0,0 +1,9 @@
+# main_app/forms.py
+from django import forms
+
+
+class CatForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    breed = forms.CharField(label='Breed', max_length=100)
+    description = forms.CharField(label='Description', max_length=250)
+    age = forms.IntegerField(label='Age')
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Cat
+from .forms import CatForm
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 from django.http import HttpResponse
@@ -7,7 +9,8 @@ from django.http import HttpResponse
 
 def index(request):
     cats = Cat.objects.all()
-    return render(request, 'index.html', {'cats': cats})
+    form = CatForm()
+    return render(request, 'index.html', {'cats': cats, 'form': form})
 
 
 def about(request):
@@ -17,6 +20,18 @@ def about(request):
 def show(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'show.html', {'cat': cat})
+
+
+def post_cat(request):
+    form = CatForm(request.POST)
+    if form.is_valid():
+        cat = Cat(
+            name=form.cleaned_data['name'],
+            breed=form.cleaned_data['breed'],
+            description=form.cleaned_data['description'],
+            age=form.cleaned_data['age'])
+        cat.save()
+    return HttpResponseRedirect('/')
 
 # class Cat:
 #     def __init__(self, name, breed, description, age):
