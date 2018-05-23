5b6941aea0ade5d04f86ea1b2aa51b59cc6d6642
Faisal Al Qasimi <falqas@gmail.com>
2018-05-23 07:27:59 -0700

    run migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# A model is the single, definitive source of information about your data. It contains the essential fields and behaviors of the data you’re storing. Generally, each model maps to a single database table.
+
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    age = models.IntegerField()
