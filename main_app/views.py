2204807dedba19cb47c07c3f10ce6daaf096fbd2
Faisal Al Qasimi <falqas@gmail.com>
2018-05-23 07:43:39 -0700

    set up admin user

--- main_app/views.py
+++ main_app/views.py
@@ -22,8 +22,8 @@ def about(request):
 #         self.age = age
 
 
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# cats = [
+#     Cat('Lolo', 'tabby', 'foul little demon', 3),
+#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#     Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
