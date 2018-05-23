955c7e7e3da230a5ef142595a2714fea37feafd7
Faisal Al Qasimi <falqas@gmail.com>
2018-05-23 15:39:12 -0700

    implement login

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post_url/', views.post_cat, name='post_cat'),
     path('user/<username>/', views.profile,
          name='profile'),  # this line is new
+    path('login/', views.login_view, name="login")
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Cat
-from .forms import CatForm
+from .forms import CatForm, LoginForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -38,6 +39,28 @@ def profile(request, username):
     user = User.objects.get(username=username)
     cats = Cat.objects.filter(user=user)
     return render(request, 'profile.html', {'username': username, 'cats': cats})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        # if post, then authenticate (user submitted username and password)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user. is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print("The account has been disabled.")
+            else:
+                print("The username and/or password is incorrect.")
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
 
 # class Cat:
 #     def __init__(self, name, breed, description, age):
