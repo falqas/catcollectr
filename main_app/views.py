a0e9c09239f565a7e34d67671d6212c38609d223
Faisal Al Qasimi <falqas@gmail.com>
2018-05-22 22:13:06 -0700

    add cats

--- main_app/views.py
+++ main_app/views.py
@@ -6,8 +6,23 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'cats': cats})
 
 
 def about(request):
     return HttpResponse('<h1>About me page!</h1>')
+
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
